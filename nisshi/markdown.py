c7f3e2f732681a5378bdc3f5e3cc95dae0eba5a7
Takagi Tasuku <tasuren_0618@yahoo.co.jp>
2022-11-19 13:45:29 +0900

    fix: mizuを使った時に動かないのを修正した。

--- nisshi/markdown.py
+++ nisshi/markdown.py
@@ -1,11 +1,12 @@
 # nisshi - Markdown
 
-try: from mizu import parse
+try: from mizu import parse_ext
 except ModuleNotFoundError: from mistletoe import markdown
 else:
     from typing import Any
     def markdown(text: str, *args: Any, **kwargs: Any) -> str:
         kwargs.setdefault("tables", True)
-        return parse(text, *args, **kwargs)
+        return parse_ext(text, *args, **kwargs)
+
 
 __all__ = ("markdown",)
